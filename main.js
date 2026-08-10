1ecdbdb720b79cca700a6eaac0a96efacd2df753
mukaratejaswini07-sketch <mukaratejaswini07@gmail.com>
2026-08-10 16:07:13 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+import { add, subtract, multiply, divide } from "./math.js";
+let a = 20;
+let b = 10;
+console.log("First Number:", a);
+console.log("Second Number:", b);
+console.log("Addition:", add(a, b));
+console.log("Subtraction:", subtract(a, b));
+console.log("Multiplication:", multiply(a, b));
+console.log("Division:", divide(a, b));
