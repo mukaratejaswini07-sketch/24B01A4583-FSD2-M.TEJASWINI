1784116f4480ba374c1dd75842c3fc2c9c66e2ef
mukaratejaswini07-sketch <mukaratejaswini07@gmail.com>
2026-09-19 09:23:17 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Student Study Corner</h1>
+
+      <h2 style={{ color: "red" }}>Welcome Students</h2>
+
+      <p>Manage your studies and learn new things every day.</p>
+
+      <h3>My Subjects</h3>
+
+      <ul>
+        <li>Mathematics</li>
+        <li>Computer Science</li>
+        <li>English</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
